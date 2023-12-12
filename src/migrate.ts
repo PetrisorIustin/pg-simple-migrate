3b34fa584aeedac8bd4beba1ac0c4524167c0fa5
Iustin Petrisor <iustinpetrisor@gmail.com>
2023-12-12 10:48:09 +0200

    added pools

--- src/migrate.ts
+++ src/migrate.ts
@@ -2,28 +2,25 @@
 
 import fs from 'fs/promises';
 import path from 'path';
-import { Client, ClientConfig } from 'pg';
-
+import { Pool, PoolClient, PoolConfig } from 'pg';
 
 export type MigrationOptions = {
-  config: string | ClientConfig | undefined;
+  config: PoolConfig | undefined;
   migrationFolder: string;
-}
+};
 
 export const migrate = async (options: MigrationOptions) => {
   if (!options.config) {
     throw new Error('No config provided');
   }
 
-  const client = new Client(options.config);
+  const pool = new Pool(options.config);
+
+  let client: PoolClient | undefined = undefined;
+
   try {
-    await client.connect();
-  } catch (error) {
-    console.error('Error connecting to database:', error);
-    return;
-  }
-  
-  try {
+    client = await pool.connect();
+
     // Create migrations table if it doesn't exist
     await client.query(`
       CREATE TABLE IF NOT EXISTS migrations (
@@ -43,20 +40,39 @@ export const migrate = async (options: MigrationOptions) => {
       const result = await client.query('SELECT id FROM migrations WHERE name = $1', [file]);
 
       if (result.rows.length === 0) {
-        console.log(`Running migration: ${file}`);
-        await client.query(migrationQuery);
+        console.log(`Running migration within transaction: ${file}`);
 
-        // Record the migration in the migrations table
-        await client.query('INSERT INTO migrations (name) VALUES ($1)', [file]);
+        // Begin a new transaction
+        await client.query('BEGIN');
+
+        try {
+          // Execute the migration
+          await client.query(migrationQuery);
+
+          // Record the migration in the migrations table
+          await client.query('INSERT INTO migrations (name) VALUES ($1)', [file]);
+
+          // Commit the transaction if everything is successful
+          await client.query('COMMIT');
+
+          console.log(`Migration completed successfully: ${file}`);
+        } catch (migrationError) {
+          // Rollback the transaction in case of an error
+          await client.query('ROLLBACK');
+          console.error(`Error executing migration: ${file}`, migrationError);
+        }
       } else {
         console.log(`Skipping already run migration: ${file}`);
       }
     }
 
-    console.log('Migrations completed successfully.');
+    console.log('All migrations completed successfully.');
   } catch (error) {
     console.error('Error running migrations:', error);
   } finally {
-    await client.end();
+    if (client) {
+      client.release();
+    }
+    await pool.end();
   }
-}
+};
