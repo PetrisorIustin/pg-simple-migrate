078563b3918c6d829a7d028651e38f722533b223
Iustin Petrisor <iustinpetrisor@gmail.com>
2023-12-12 11:15:53 +0200

    Added schema

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,5 +1,3 @@
-// migrate.ts
-
 import fs from 'fs/promises';
 import path from 'path';
 import { Pool, PoolClient, PoolConfig } from 'pg';
@@ -7,12 +5,15 @@ import { Pool, PoolClient, PoolConfig } from 'pg';
 export type MigrationOptions = {
   config: PoolConfig | undefined;
   migrationFolder: string;
+  schema?: string; // Make schema optional
 };
 
 export const migrate = async (options: MigrationOptions) => {
   if (!options.config) {
     throw new Error('No config provided');
   }
+
+  const schema = options.schema || 'simple_migrate'; // Use 'simple_migrate' as the default schema
 
   const pool = new Pool(options.config);
 
@@ -21,9 +22,9 @@ export const migrate = async (options: MigrationOptions) => {
   try {
     client = await pool.connect();
 
-    // Create migrations table if it doesn't exist
+    // Create migrations table in the specified schema if it doesn't exist
     await client.query(`
-      CREATE TABLE IF NOT EXISTS migrations (
+      CREATE TABLE IF NOT EXISTS ${schema}.migrations (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         timestamp TIMESTAMP DEFAULT NOW()
@@ -37,7 +38,10 @@ export const migrate = async (options: MigrationOptions) => {
       const migrationQuery = await fs.readFile(filePath, 'utf8');
 
       // Check if the migration has been run
-      const result = await client.query('SELECT id FROM migrations WHERE name = $1', [file]);
+      const result = await client.query(
+        `SELECT id FROM ${schema}.migrations WHERE name = $1`,
+        [file]
+      );
 
       if (result.rows.length === 0) {
         console.log(`Running migration within transaction: ${file}`);
@@ -50,7 +54,10 @@ export const migrate = async (options: MigrationOptions) => {
           await client.query(migrationQuery);
 
           // Record the migration in the migrations table
-          await client.query('INSERT INTO migrations (name) VALUES ($1)', [file]);
+          await client.query(
+            `INSERT INTO ${schema}.migrations (name) VALUES ($1)`,
+            [file]
+          );
 
           // Commit the transaction if everything is successful
           await client.query('COMMIT');
